0875f878a45f2a7302e3bd3b66ed5a2a313d8e26
akashabdulmazid <akashabdulmazid@gmail.com>
2025-12-11 19:38:27 +0600

    404 route create

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,13 @@ const app = express();
 //parser
 app.use(express.json());
 
-
+// 404 route
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'route not found',
+        path: req.path,
+    })
+});
 
 export default app;
